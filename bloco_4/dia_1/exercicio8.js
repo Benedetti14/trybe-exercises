a57223f949fddcceef36360fbe32699f3eba91c2
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-06-23 19:08:56 -0300

    Insere o arquivo exercicio8.js e resolve o exercicio 8

---
+++ bloco_4/dia_1/exercicio8.js
@@ -0,0 +1,20 @@
+let n1 = 1;
+let n2 = 9;
+let n3 = 8;
+
+if(n1%2 == 0){
+  console.log('true');
+}
+else{
+  if(n2%2 == 0){
+    console.log('true');
+  }
+  else{
+    if(n3%2 == 0){
+      console.log('true');
+    }
+    else{
+      console.log('false');
+    }
+  }
+}
