c81a82cef346caa8ec04c9923b51de615617edbe
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-06-23 18:39:00 -0300

    Insere o arquivo exercicio5.js e resolve o exercicio 5

---
+++ bloco_4/dia_1/exercicio5.js
@@ -0,0 +1,17 @@
+let ang1 = 90;
+let ang2 = 45;
+let ang3 = 45;
+
+
+if(ang1<0 || ang2 <0 || ang3<0){
+  console.log('Erro');
+} 
+else{
+  if (ang1+ang2+ang3 == 180){
+    console.log('true');
+  }
+  else{
+    console.log('false');
+  }
+}
+
