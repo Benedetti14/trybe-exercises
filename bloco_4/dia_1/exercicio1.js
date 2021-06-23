6f7fed9bbff337ec5dd03fe4dcd9e14523822849
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-06-23 18:28:36 -0300

    Exercicio 2 e 3

--- bloco_4/dia_1/exercicio1.js
+++ bloco_4/dia_1/exercicio1.js
@@ -1,5 +1,6 @@
 let a = 2;
 let b = 10;
+let c = 18;
 
 // Adição
 console.log(a+b);
@@ -15,3 +16,24 @@ console.log(a/b);
 
 // Módulo
 console.log(a%b);
+
+// Exercicio 2 
+
+if (a>b){
+  console.log(a);
+}
+else{
+  console.log(b);
+}
+
+// Exercicio 3
+
+if (a>b && a>c){
+  console.log(a);
+}
+else if(b>a && b>c){
+  console.log(b);
+}
+else {
+  console.log(c);
+}
