92880d8acc984b9d3dd19b2d5b8d5487a535dd5e
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-06-23 18:24:49 -0300

    Exercicio 1

---
+++ bloco_4/dia_1/exercicio1.js
@@ -0,0 +1,17 @@
+let a = 2;
+let b = 10;
+
+// Adição
+console.log(a+b);
+
+// Subtração
+console.log(a-b);
+
+//Multiplicação
+console.log(a*b);
+
+// Divisão
+console.log(a/b);
+
+// Módulo
+console.log(a%b);
