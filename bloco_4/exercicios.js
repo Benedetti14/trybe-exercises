dfac7d7427f3afef1bc600babd7b9bafbb27c581
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-06-24 16:10:11 -0300

    exercicio 5 e 6

--- bloco_4/exercicios.js
+++ bloco_4/exercicios.js
@@ -2,6 +2,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let i;
 let sum = 0;
 let med = 0;
+let maior = 0;
+let impares = 0;
 
 for (i=0; i<numbers.length; i+=1){
   console.log(numbers[i]);
@@ -24,4 +26,23 @@ else{
 
 console.log(med);
 
+for(i=0; i<numbers.length; i+=1){
+  if(numbers[i]>maior){
+    maior = numbers[i];
+  }
+}
 
+console.log(maior);
+
+for(i=0; i<numbers.length; i+=1){
+  if(numbers[i]%2 != 0){
+    impares+=1;
+  }
+}
+
+if(impares == 0){
+  console.log('nenhum valor ímpar encontrado');
+}
+else{
+  console.log('Existem ' + impares + ' numeros impares');
+}
