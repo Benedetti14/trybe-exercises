f70cee4578d0e3c328ae7bb9b2ae94835f1822c9
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-06-24 16:05:10 -0300

    exercicio 3 e 4

--- bloco_4/exercicios.js
+++ bloco_4/exercicios.js
@@ -1,6 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let i;
 let sum = 0;
+let med = 0;
 
 for (i=0; i<numbers.length; i+=1){
   console.log(numbers[i]);
@@ -11,3 +12,16 @@ for (i=0; i<numbers.length; i+=1){
 }
 
 console.log(sum);
+
+med = sum/numbers.length;
+
+if(med>20){
+  console.log('valor maior que 20');
+}
+else{
+  console.log('valor menor que 20');
+}
+
+console.log(med);
+
+
