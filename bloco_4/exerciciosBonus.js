4b852356ac217a79f71aaa89714aba4fd9959717
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-06-24 16:46:02 -0300

    exercicio bonus 3

--- bloco_4/exerciciosBonus.js
+++ bloco_4/exerciciosBonus.js
@@ -24,3 +24,18 @@ for(i=1; i < numbers.length; i+=1){
 }
 
 console.log(numbers);
+
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let numbersM = [];
+
+for(i=0;i<numbers.length; i+=1){
+  if(i==numbers.length - 1){
+    numbersM.push(numbers[i]*2);
+  }
+  else{
+    numbersM.push(numbers[i]*numbers[i+1]);
+  }
+}
+
+console.log(numbersM)
