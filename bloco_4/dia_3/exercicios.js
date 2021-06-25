04dcf380908b6c012514243986af2f50f6e09de6
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-06-25 16:05:29 -0300

    exercicios 5 e arruma o exercicio 4

--- bloco_4/dia_3/exercicios.js
+++ bloco_4/dia_3/exercicios.js
@@ -1,4 +1,4 @@
-let n = 5;
+let n = 7;
 
 // for(let i = 0; i<n; i+=1){
 //   console.log('*'.repeat(n));
@@ -14,22 +14,38 @@ let n = 5;
 // }
 
 
-for(let i = 0; i<n; i+=1){
-  if(n%2 === 0){
-    if((i+1)%2 === 0){
-      console.log(' '.repeat((n-(i+1))/2) + '*'.repeat(i+1) + ' '.repeat((n-(i+1))/2));
-    }
-    else{
-      console.log();
-    }
+// for(let i = 0; i<n; i+=1){
+//   if(n%2 === 0){
+//     if((i+1)%2 === 0){
+//       console.log(' '.repeat((n-(i+1))/2) + '*'.repeat(i+1) + ' '.repeat((n-(i+1))/2));
+//     }
+//     else{
+//     }
 
+//   }
+//   else{
+//     if((i+1)%2 === 0){
+//     }
+//     else{
+//       console.log(' '.repeat((n-(i+1))/2) + '*'.repeat(i+1) + ' '.repeat((n-(i+1))/2));
+//     }
+//   }
+// }
+
+for(let i = 0; i<n; i+=1){
+  if(i+1 === n){
+    console.log('*'.repeat(n))
   }
   else{
     if((i+1)%2 === 0){
-      console.log();
     }
     else{
-      console.log(' '.repeat((n-(i+1))/2) + '*'.repeat(i+1) + ' '.repeat((n-(i+1))/2));
+      if(i+1 === 1){
+        console.log(' '.repeat((n-(i+1))/2) + '*'.repeat(i+1) + ' '.repeat((n-(i+1))/2));
+      }
+      else{
+        console.log(' '.repeat((n-(i+1))/2) + '*'.repeat(1) + ' '.repeat((i+1) - 2) + '*'.repeat(1) + ' '.repeat((n-(i+1))/2));
+      }
     }
   }
 }
