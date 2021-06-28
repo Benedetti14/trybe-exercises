763487ca500649a958a4f21622f1b07a6b0e465d
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-06-28 16:58:07 -0300

    Exercicio 5

--- bloco_4/dia_4/exercicios.js
+++ bloco_4/dia_4/exercicios.js
@@ -4,16 +4,31 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-console.log("Bem-vinda, " + info.personagem);
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+
+// console.log("Bem-vinda, " + info.personagem);
 
 info['recorrente'] = 'Sim';
 
-console.log(info);
+// console.log(info);
+
+// for(let key in info){
+//   console.log(key);
+// }
+
+// for(let key in info){
+//   console.log(info[key]);
+// }
 
 for(let key in info){
-  console.log(key);
-}
-
-for(let key in info){
-  console.log(info[key]);
+  if(info[key] === info2[key]){
+    console.log("Ambos recorrentes");
+  }else{
+  console.log(info[key] + ' e ' + info2[key]);
+  }
 }
