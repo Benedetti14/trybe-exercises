29ea96429a5b8cb32744aec514c8356612e896fa
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-06-28 17:43:55 -0300

    Exercicio 2 parte II

--- bloco_4/dia_4/exerciciosParteII.js
+++ bloco_4/dia_4/exerciciosParteII.js
@@ -1,13 +1,30 @@
-function verificaPalindrome (str){
-  let verifica = true;
-  let aux = str.length -1;
-  for(let i = 0; i<str.length; i+=1){
-    if(str[i] != str[aux - i]){
-      verifica = false;
-      break;
+// Exercicio 1 
+// function verificaPalindrome (str){
+//   let verifica = true;
+//   let aux = str.length -1;
+//   for(let i = 0; i<str.length; i+=1){
+//     if(str[i] != str[aux - i]){
+//       verifica = false;
+//       break;
+//     }
+//   }
+//   return verifica;
+// }
+
+// console.log(verificaPalindrome('arara'.toLocaleLowerCase()));
+
+/***************************************************/
+
+// Exercicio 2
+
+function maior(vet){
+  let maior = 0;
+  for(let i = 0; i<vet.length; i+=1){
+    if (vet[i] > maior){
+      maior = vet[i];
     }
   }
-  return verifica;
+  return maior;
 }
-
-console.log(verificaPalindrome('arara'.toLocaleLowerCase()));
+let teste = [2, 3, 6, 7, 10, 1]
+console.log(maior(teste));
