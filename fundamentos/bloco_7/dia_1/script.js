11c0ab94f140f807fcd391cb76dc9899a9875831
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-07-22 19:01:35 -0300

    exercicio 2

--- fundamentos/bloco_7/dia_1/script.js
+++ fundamentos/bloco_7/dia_1/script.js
@@ -1,14 +1,20 @@
 //Exercicio 1
 
-const testingScope = (escopo)  => {
-  if (escopo === true) {
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = `${ifScope}. Ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
-}
+// const testingScope = (escopo)  => {
+//   if (escopo === true) {
+//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = `${ifScope}. Ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+// }
 
-testingScope(false);
+// testingScope(false);
+
+//Exercicio 2
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!` ); // será necessário alterar essa linha 😉
