704e5a13402f04e3bd902f7212e2cce732e470c6
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-07-22 19:33:54 -0300

    exercicio 1 e 2 da parte 2

--- fundamentos/bloco_7/dia_1/script.js
+++ fundamentos/bloco_7/dia_1/script.js
@@ -1,3 +1,5 @@
+//Parte 1
+
 //Exercicio 1
 
 // const testingScope = (escopo)  => {
@@ -15,6 +17,31 @@
 
 //Exercicio 2
 
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!` ); // será necessário alterar essa linha 😉
+// console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!` ); // será necessário alterar essa linha 😉
+
+//Parte 2
+
+//Exercicio 1
+
+// let fat = 1;
+
+// const fatorial = (num) => num === 0 ? fat : fat *= num * fatorial(num - 1);
+
+// console.log(fatorial(6));
+
+//Exercicio 2
+
+const longestWord = (word) => {
+  let words = word.split(' ');
+  let maior = 0;
+  let w = ''
+  for (let i = 0; i < words.length; i += 1){
+    words[i].length > maior ?  maior = words[i].length : maior;
+
+    words[i].length === maior ? w = words[i] : w;
+  }
+  return w;
+}
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"))
