9b1a4caa470405b3a7fb6a910780a21eaad045ef
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-07-28 15:56:55 -0300

    Exercicio 1

---
+++ fundamentos/bloco_8/dia_1/script.js
@@ -0,0 +1,17 @@
+//Exercicio 1
+
+const employee = (fullName) => {
+  const emailName = fullName.toLowerCase().replace(' ', '_');
+  return {nomecompleto: fullName, email: `${emailName}@trybe.com`
+}};
+
+const newEmployees = () => {
+  const employees = {
+    id1: employee('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: employee('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: employee('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees());
