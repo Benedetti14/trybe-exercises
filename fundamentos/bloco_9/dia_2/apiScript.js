b9aa296419c18577027a71dcb84c197cbf3c2fa5
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-08-13 19:16:02 -0300

    Exercicios do dia 9.2

---
+++ fundamentos/bloco_9/dia_2/apiScript.js
@@ -0,0 +1,20 @@
+// apiScript.js
+const API_URL = 'https://icanhazdadjoke.com/';
+
+
+const fetchJoke = () => {
+  // Adicionar lógica aqui!
+  const joke = document.getElementById('jokeContainer');
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  }; 
+
+  fetch(API_URL, myObject)
+    .then((response) => response.json())
+    .then((data) => {
+      joke.innerHTML = data.joke;
+    });
+};
+
+window.onload = () => fetchJoke();
